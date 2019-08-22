0f58700329ef0519f827485aca4735f3e08c0643
Paul Caston <paul.caston@iag.com.au>
2019-08-22 16:58:55 +0800

    standard login

--- openpeerpower/components/websocket_api/auth.py
+++ openpeerpower/components/websocket_api/auth.py
@@ -82,35 +82,6 @@ class AuthPhase:
                 return await self._async_finish_auth(
                     refresh_token.user, refresh_token)
 
-        if msg['type'] == 'register':
-            provider = _async_get_opp_provider(self._opp)
-            await provider.async_initialize()   
-            user = await self._opp.auth.async_create_user(
-                name=msg['name'],
-            )
-            await self._opp.async_add_executor_job(
-                provider.data.add_auth, msg['username'], msg['password'])
-            credentials = await provider.async_get_or_create_credentials({
-                'username': msg['username']
-            })
-            await provider.data.async_save()
-            await self._opp.auth.async_link_user(user, credentials)
-            if 'person' in self._opp.config.components:
-                await self._opp.components.person.async_create_person(
-                    data['name'], user_id=user.id
-                )
-            # Return authorization token for fetching tokens and connect
-
-            refresh_token = await self._opp.auth.async_create_refresh_token(
-                user, msg['client_id'],msg['username'],
-                token_type = 'long_lived_access_token',
-                access_token_expiration=timedelta(days=365)
-                )
-             
-            if refresh_token is not None:
-                return await self._async_finish_auth(
-                    refresh_token.user, refresh_token)
-
         if msg['type'] == 'login':
             provider = _async_get_opp_provider(self._opp)
             try:
@@ -131,24 +102,6 @@ class AuthPhase:
                         refresh_token.user, refresh_token)
             except InvalidAuth:
                 print("Auth invalid")
-
-        if msg['type'] == 'rogin':
-            provider = _async_get_opp_provider(self._opp)
-            try:
-                await provider.async_validate_login(
-                    msg['username'], msg['api_password'])
-
-                # password is valid return authorization token for fetching tokens and connect
-                user = await self._opp.auth.async_get_user_by_credentials(
-                    await provider.async_get_or_create_credentials({
-                    'username': msg['username']
-                    })
-                )
-                for refresh_token in user.refresh_tokens.values():
-                    if refresh_token.client_name == msg['username']:
-                        break
-            except InvalidAuth:
-                print("Auth invalid")
                 return
             except NoUsers:
                 await provider.async_initialize()   
@@ -156,7 +109,7 @@ class AuthPhase:
                     name=msg['name'],
                 )
                 await self._opp.async_add_executor_job(
-                    provider.data.add_auth, msg['username'], msg['password'])
+                    provider.data.add_auth, msg['username'], msg['api_password'])
                 credentials = await provider.async_get_or_create_credentials({
                     'username': msg['username']
                 })
@@ -173,7 +126,6 @@ class AuthPhase:
                     token_type = 'long_lived_access_token',
                     access_token_expiration=timedelta(days=365)
                     )
-            finally:
                 if refresh_token is not None:
                     return await self._async_finish_auth(
                         refresh_token.user, refresh_token)
--- openpeerpower/testjwt.py
+++ openpeerpower/testjwt.py
@@ -9,14 +9,16 @@ import asyncws
 import os
 
 
-#fName = 'C:\\Users\\s69171\\AppData\\Roaming\\.openpeerpower\\access_token.txt'
-fName = 'C:\\Users\\Paul\\AppData\\Roaming\\.openpeerpower\\access_token.txt'
+fName = 'C:\\Users\\s69171\\AppData\\Roaming\\.openpeerpower\\access_token.txt'
+opp = 'C:\\Users\\s69171\\AppData\\Roaming\\.openpeerpower\\opp.txt'
+#fName = 'C:\\Users\\Paul\\AppData\\Roaming\\.openpeerpower\\access_token.txt'
 AT = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIwNTY1ZmIyMTJjZDY0OWUzODE3OWE1NjVkNWI2MzI0ZSIsImlhdCI6MTU2NjMwNDI5MSwiZXhwIjoxNTY2MzA2MDkxfQ.scict7xxyhreFxuUYpHN90lf_ttYh3-t40YGZsHvgfQ'
 
 
 async def main():
     """Simple WebSocket client """
-    websocket = await asyncws.connect('ws://localhost:8123/api/websocket')
+    # websocket = await asyncws.connect('ws://localhost:8123/api/websocket')
+    websocket = await asyncws.connect('ws://localhost:8123')
     while True:
         message = await websocket.recv()
         msg = json.loads(message)
@@ -31,38 +33,26 @@ async def main():
                 ))
             else:
                 await websocket.send(json.dumps(
-                #{ 'type': 'register', 'client_id': 'http://127.0.0.1:8081', 'name': 'Paul', 'username': 'paul','password': 'Boswald0'}
-                #))
-                { 'type': 'rogin', 'client_id': 'http://127.0.0.1:8081', 'name': 'Paul', 'username': 'paul','api_password': 'Boswald0'}
+                { 'type': 'login', 'client_id': 'http://127.0.0.1:8081', 'name': 'Paul', 'username': 'paul','api_password': 'Boswald0'}
                 ))
         
         if msg['type'] == 'auth_ok':
-            if os.path.exists(fName):
-                await websocket.send(json.dumps(
-                {'id': 2, 'type': 'get_states'}
-                ))
-            else:
-                if msg['access_token']:
-                    with open(fName, 'w') as f:
-                        f.write(msg['access_token'])
-                #await websocket.send(json.dumps(
-                #{"id": 1, "type": "auth/long_lived_access_token", "client_name": "paul", "client_icon": '', "lifespan": 365}
+            if not os.path.exists(fName):
+                with open(fName, 'w') as f:
+                    f.write(msg['access_token'])
+            await websocket.send(json.dumps(
+            {'id': 1, 'type': 'get_states'}
+            ))
+
+            #await websocket.send(json.dumps(
+            #{"id": 1, "type": "auth/long_lived_access_token", "client_name": "paul", "client_icon": '', "lifespan": 365}
             #{'id': 2, 'type': 'subscribe_events', 'event_type': 'state_changed'}
             #))
 
         if msg['type'] == 'result' and msg['id'] == 1:
-            ACCESS_TOKEN = msg['result']
-            if os.path.exists(fName):
-                pass
-            else:
-                with open(fName, 'w') as f:
-                    f.write(ACCESS_TOKEN)
-            await websocket.send(json.dumps(
-            {'id': 2, 'type': 'get_states'}
-            ))
-
-        if msg['type'] == 'result' and msg['id'] == 2:
-            print(message)
+            if not os.path.exists(opp):
+                with open(opp, 'w') as f:
+                    f.write(message)
             break
 
         print(message)
